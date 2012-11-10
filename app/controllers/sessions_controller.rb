397c5b7ac7c874654a03aff6071543fcf5f1b827
Joe Villanueva <spcorbust@hotmail.com>
2012-11-10 15:59:43 -0600

    Changes before meeting.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url
+      redirect_to users_url(params[:id])
     else
       # redirect_to root_url, :notice => "Nice try!"
       render 'new'
