4cd4228b03ab9f76d4a423cbef5e471e63e8e373
Tomas Gregor <tomas.gregor@me.com>
2012-11-19 16:03:51 -0600

    TG: added column description:text to Projects table

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :desc, :name, :project_url, :status, :user_id
+  attr_accessible :description, :name, :project_url, :status, :user_id
   belongs_to :user
   has_many :shots
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121119220016) do
+ActiveRecord::Schema.define(:version => 20121119220237) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121119220016) do
     t.string   "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
   create_table "shots", :force => true do |t|
