d44c4cd19b6453a3564bcdaedcdb6ce6b7ad5301
Tomas Gregor <tomas.gregor@me.com>
2012-11-19 18:36:04 -0600

    TG:cleaned User model - some validations were duplication of  has_secure_password method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ActiveRecord::Base
   
-  attr_accessible :avatar_url, :email, :first_name, :last_name, :password, :password_confirmation, :password_digest, :status, :type, :admin
+  attr_accessible :avatar_url, :email, :first_name, :last_name, :password, :password_confirmation, :status, :type, :admin
   has_many :projects
   has_many :shots, :through => :projects
 
   has_secure_password
   
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
+  validates_presence_of :email, :first_name, :last_name
   validates_uniqueness_of :email
 
 end
