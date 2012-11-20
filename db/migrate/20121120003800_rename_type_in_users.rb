f4a711330f731dbb88adb9f757b438ec775bb34f
Tomas Gregor <tomas.gregor@me.com>
2012-11-19 18:41:25 -0600

    TG: renamed Type in Users to Category. Type as a column name can cause conflicts with some Rails stuff

---
+++ db/migrate/20121120003800_rename_type_in_users.rb
@@ -0,0 +1,9 @@
+class RenameTypeInUsers < ActiveRecord::Migration
+  def up
+    rename_column :users, :type, :category
+  end
+
+  def down
+    rename_column :users, :category, :type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121119220237) do
+ActiveRecord::Schema.define(:version => 20121120003800) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(:version => 20121119220237) do
     t.string   "password_digest"
     t.datetime "created_at",                               :null => false
     t.datetime "updated_at",                               :null => false
-    t.string   "type"
+    t.string   "category"
     t.string   "status"
     t.boolean  "admin",                 :default => false
   end
